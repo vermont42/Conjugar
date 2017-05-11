efe12459f09692d1764184e17939844288899b77
vermont42 <vermontlawyer@gmail.com>
2017-05-11 09:54:12 -1000

    finish VerbViewController

--- Conjugar/VerbViewController.swift
+++ Conjugar/VerbViewController.swift
@@ -75,7 +75,7 @@ class VerbViewController: UIViewController, UITableViewDelegate, UITableViewData
   }
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 21 //138
+    return 138
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -99,6 +99,91 @@ class VerbViewController: UIViewController, UITableViewDelegate, UITableViewData
     case 15...20:
       tense = .imperfectoDeIndicativo
       personNumber = personNumbers[row - 15]
+    case 21:
+      tense = .futuroDeIndicativo
+    case 22...27:
+      tense = .futuroDeIndicativo
+      personNumber = personNumbers[row - 22]
+    case 28:
+      tense = .condicional
+    case 29...34:
+      tense = .condicional
+      personNumber = personNumbers[row - 29]
+    case 35:
+      tense = .presenteDeSubjuntivo
+    case 36...41:
+      tense = .presenteDeSubjuntivo
+      personNumber = personNumbers[row - 36]
+    case 42:
+      tense = .futuroDeSubjuntivo
+    case 43...48:
+      tense = .futuroDeSubjuntivo
+      personNumber = personNumbers[row - 43]
+    case 49:
+      tense = .imperfectoDeSubjuntivo1
+    case 50...55:
+      tense = .imperfectoDeSubjuntivo1
+      personNumber = personNumbers[row - 50]
+    case 56:
+      tense = .imperfectoDeSubjuntivo2
+    case 57...62:
+      tense = .imperfectoDeSubjuntivo2
+      personNumber = personNumbers[row - 57]
+    case 63:
+      tense = .imperativo
+    case 64...68:
+      tense = .imperativo
+      personNumber = personNumbers[row - 63]
+    case 69:
+      tense = .imperativoNegativo
+    case 70...74:
+      tense = .imperativoNegativo
+      personNumber = personNumbers[row - 69]
+    case 75:
+      tense = .perfectoDeIndicativo
+    case 76...81:
+      tense = .perfectoDeIndicativo
+      personNumber = personNumbers[row - 76]
+    case 82:
+      tense = .preteritoAnterior
+    case 83...88:
+      tense = .preteritoAnterior
+      personNumber = personNumbers[row - 83]
+    case 89:
+      tense = .pluscuamperfectoDeIndicativo
+    case 90...95:
+      tense = .pluscuamperfectoDeIndicativo
+      personNumber = personNumbers[row - 90]
+    case 96:
+      tense = .futuroPerfecto
+    case 97...102:
+      tense = .futuroPerfecto
+      personNumber = personNumbers[row - 97]
+    case 103:
+      tense = .condicionalCompuesto
+    case 104...109:
+      tense = .condicionalCompuesto
+      personNumber = personNumbers[row - 104]
+    case 110:
+      tense = .perfectoDeSubjuntivo
+    case 111...116:
+      tense = .perfectoDeSubjuntivo
+      personNumber = personNumbers[row - 111]
+    case 117:
+      tense = .pluscuamperfectoDeSubjuntivo1
+    case 118...123:
+      tense = .pluscuamperfectoDeSubjuntivo1
+      personNumber = personNumbers[row - 118]
+    case 124:
+      tense = .pluscuamperfectoDeSubjuntivo2
+    case 125...130:
+      tense = .pluscuamperfectoDeSubjuntivo2
+      personNumber = personNumbers[row - 125]
+    case 131:
+      tense = .futuroPerfectoDeSubjuntivo
+    case 132...137:
+      tense = .futuroPerfectoDeSubjuntivo
+      personNumber = personNumbers[row - 132]
     default:
       fatalError()
     }
