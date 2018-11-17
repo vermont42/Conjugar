30cc74627449a5ed004765a3cf05d1b21704706d
vermont42 <vermontcoder@gmail.com>
2018-11-17 07:43:44 -0800

    remove extra space

--- Conjugar/MutableCollectionExtension.swift
+++ Conjugar/MutableCollectionExtension.swift
@@ -13,7 +13,7 @@ extension MutableCollection {
     let c = count
     guard c > 1 else { return }
     
-    for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+    for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
       let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
       let i = index(firstUnshuffled, offsetBy: d)
       swapAt(firstUnshuffled, i)
