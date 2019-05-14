858b21187913007a6da51e8651d4dcbfc45e6631
vermont42 <vermontcoder@gmail.com>
2019-05-14 07:08:31 -0700

    add unit tests

---
+++ ConjugarTests/Models/PersonNumberTests.swift
@@ -0,0 +1,50 @@
+//
+//  PersonNumberTests.swift
+//  ConjugarTests
+//
+//  Created by Joshua Adams on 5/13/19.
+//  Copyright © 2019 Josh Adams. All rights reserved.
+//
+
+import XCTest
+@testable import Conjugar
+
+class PersonNumberTests: XCTestCase {
+  func testShortDisplayName() {
+    var personNumber = PersonNumber.firstSingular
+    XCTAssertEqual(personNumber.shortDisplayName, "1S")
+    personNumber = .secondSingularTu
+    XCTAssertEqual(personNumber.shortDisplayName, "2S")
+    personNumber = .secondSingularVos
+    XCTAssertEqual(personNumber.shortDisplayName, "2SV")
+    personNumber = .thirdSingular
+    XCTAssertEqual(personNumber.shortDisplayName, "3S")
+    personNumber = .firstPlural
+    XCTAssertEqual(personNumber.shortDisplayName, "1P")
+    personNumber = .secondPlural
+    XCTAssertEqual(personNumber.shortDisplayName, "2P")
+    personNumber = .thirdPlural
+    XCTAssertEqual(personNumber.shortDisplayName, "3P")
+    personNumber = .none
+    XCTAssertEqual(personNumber.shortDisplayName, "none")
+  }
+
+  func testPronoun() {
+    var personNumber = PersonNumber.firstSingular
+    XCTAssertEqual(personNumber.pronoun, "yo")
+    personNumber = PersonNumber.secondSingularTu
+    XCTAssertEqual(personNumber.pronoun, "tú")
+    personNumber = PersonNumber.secondSingularVos
+    XCTAssertEqual(personNumber.pronoun, "vos")
+    personNumber = PersonNumber.thirdSingular
+    XCTAssertEqual(personNumber.pronoun, "él")
+    personNumber = PersonNumber.firstPlural
+    XCTAssertEqual(personNumber.pronoun, "nosotros")
+    personNumber = PersonNumber.secondPlural
+    XCTAssertEqual(personNumber.pronoun, "vosotros")
+    personNumber = PersonNumber.thirdPlural
+    XCTAssertEqual(personNumber.pronoun, "ellas")
+    personNumber = PersonNumber.none
+    XCTAssertEqual(personNumber.pronoun, "none")
+  }
+}
