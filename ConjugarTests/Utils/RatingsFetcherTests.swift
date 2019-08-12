33f3df825350913f551c941dfaa62058934210d1
vermont42 <vermontcoder@gmail.com>
2019-08-11 20:20:10 -0700

    add verbs; fix test; bump version

--- ConjugarTests/Utils/RatingsFetcherTests.swift
+++ ConjugarTests/Utils/RatingsFetcherTests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 class RatingsFetcherTests: XCTestCase {
   func testNoReviews() {
-    testDescription(count: 0, expectedDescription: "No one has rated this version of Conjugar. Be the first!")
+    testDescription(count: 0, expectedDescription: "No one has rated this version of Conjugar. ¡Sé la primera o el primero!")
   }
 
   func testOneReview() {
